f5302d3113471ade1b4c7cdbfb3ab9b2159cc1b5
Iosim <Liviu.Iosim@harman.com>
2020-04-30 00:39:12 +0300

    extracted number of tests passed, testCode respone includes string results and int noPassedTests

--- backend/controllers/codeQuestions.js
+++ backend/controllers/codeQuestions.js
@@ -10,15 +10,22 @@ function testCode(req, res) {
   let sessionId = req.session.id;
   let importInputCode = `from input_code${sessionId} import solution\n`;
   let code = req.body.answer;
-  let test = importInputCode + req.body.test;  
+  let test = importInputCode + req.body.test;
 
   try {
     fs.writeFileSync(path.join(__dirname, CODE_FOLDER, `input_code${sessionId}.py`), code);
     fs.writeFileSync(path.join(__dirname, CODE_FOLDER, `test${sessionId}.py`), test);
     const proc = exec(`python code/test${sessionId}.py`);//path.join(CODE_FOLDER, "test.py"));//
     const results = proc.toString();
+    
+    let regexPassedTests = /Passed (\d+) out/g;
+    var match = regexPassedTests.exec(results);
+    let noPassedTests = match[1];
 
-    return res.send(results);
+    return res.send({
+      results: results,
+      noPassedTests: noPassedTests
+    });
   } catch (error) {
     console.log("An error occurred");
     console.log(error);
