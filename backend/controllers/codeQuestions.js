496a88179a3d2922abe26319b7ed01f496a3308b
Iosim <Liviu.Iosim@harman.com>
2020-04-30 00:16:26 +0300

    tests work per participant using session Id

--- backend/controllers/codeQuestions.js
+++ backend/controllers/codeQuestions.js
@@ -7,24 +7,26 @@ const CODE_FOLDER = "../code";
 
 //NOTE: extract the  number of passed tests from the results string, put it in a variable, tests.result += NoPassedTests * codeQuestions.scorePerTest
 function testCode(req, res) {
-    let code = req.body.answer;
-    let test = req.body.test;
-  
-    try {
-      fs.writeFileSync(path.join(__dirname, CODE_FOLDER, "input_code.py"), code);
-      fs.writeFileSync(path.join(__dirname, CODE_FOLDER, "test.py"), test);
-      const proc = exec("python code/test.py");//path.join(CODE_FOLDER, "test.py"));//
-      const results = proc.toString();
-  
-      return res.send(results);
-    } catch (error) {
-      console.log("An error occurred");
-      console.log(error);
-  
-      return res.send("An error occurred.");
-    }
+  let sessionId = req.session.id;
+  let importInputCode = `from input_code${sessionId} import solution\n`;
+  let code = req.body.answer;
+  let test = importInputCode + req.body.test;  
+
+  try {
+    fs.writeFileSync(path.join(__dirname, CODE_FOLDER, `input_code${sessionId}.py`), code);
+    fs.writeFileSync(path.join(__dirname, CODE_FOLDER, `test${sessionId}.py`), test);
+    const proc = exec(`python code/test${sessionId}.py`);//path.join(CODE_FOLDER, "test.py"));//
+    const results = proc.toString();
+
+    return res.send(results);
+  } catch (error) {
+    console.log("An error occurred");
+    console.log(error);
+
+    return res.send("An error occurred.");
   }
+}
 
 module.exports = {
-    testCode
+  testCode
 }
