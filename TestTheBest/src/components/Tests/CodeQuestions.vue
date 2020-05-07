76fb1b87f685f75ed9af5d313e9de71f957ce37b
Iosim <Liviu.Iosim@harman.com>
2020-05-07 16:22:51 +0300

    initial interface for questions and code questions

---
+++ TestTheBest/src/components/Tests/CodeQuestions.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="codeQuestion q-ma-lg">
+      <q-card class="question">
+        <q-card-section>
+          <div class="text-h6">{{ question }}</div>
+           <q-btn color="primary" size="24px" >Run</q-btn>
+        </q-card-section>
+         
+      </q-card>
+
+      <MonacoEditor
+        theme="vs-dark"
+        language="python"
+        :options="options"
+        @change="onChange"
+        class="editor"
+        height="500px"
+      ></MonacoEditor>
+
+      <q-card class="output">
+        <q-card-section>
+          <div class="text-h6">{{ info }}</div>
+        </q-card-section>
+      </q-card>
+    </div>
+</template>
+
+<script>
+import MonacoEditor from "monaco-editor-vue";
+
+export default {
+  name: "CodeQuestions",
+  components: {
+    MonacoEditor
+  },
+  data() {
+    return {
+      question:
+        "Find the maximum number in an array of integers. For example, in the array [1, -3, 5], the maximum number is 5.",
+      info: "Passed 2 out of 2",
+      options: {
+        //Monaco Editor Options
+      }
+      
+    };
+  },
+  methods: {
+    onChange(value) {
+      console.log(value);
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.codeQuestion {
+  width: 65%;
+}
+.editor {
+  width: 100%;
+}
+.question {
+  width: 100%;
+}
+.output {
+  width: 100%;
+}
+</style>
--- TestTheBest/src/pages/Test.vue
+++ TestTheBest/src/pages/Test.vue
@@ -1,74 +1,25 @@
 <template>
-  <div class="no-scroll">
-    <div class="container0 full-width row">
-      <q-card dark bordered class="bg-grey-9 my-card col-2">
-        <q-card-section>
-          <div class="text-h6">Our Changing Planet</div>
-          <div class="text-subtitle2">by John Doe</div>
-        </q-card-section>
-
-        <q-separator dark inset />
-
-        <q-card-section>
-          {{ question }}
-        </q-card-section>
-      </q-card>
-      <div class="col">
-        <div class="container1 column">
-          <MonacoEditor
-            theme="vs-dark"
-            language="python"
-            :options="options"
-            @change="onChange"
-            class="col"
-          ></MonacoEditor>
-          <q-card dark bordered class="col-2 bg-grey-9 my-card">
-            <q-card-section>
-              <div class="text-h6">{{ info }}</div>
-              <div class="absolute-top-right text-center q-mt-sm q-mr-sm">
-                <q-btn color="secondary" size="24px" icon="play_arrow"
-                  >Run
-                </q-btn>
-              </div>
-            </q-card-section>
-          </q-card>
-        </div>
-      </div>
-    </div>
+  <div class="container row">
+    <Questions/>    
+    <CodeQuestions />
   </div>
 </template>
 
-<style scoped>
-.container0 {
-  height: 94vh;
-}
-.container1 {
-  height: 100%;
-}
-</style>
-
 <script>
-import MonacoEditor from "monaco-editor-vue";
-
+import CodeQuestions from "../components/Tests/CodeQuestions.vue";
+import Questions from "../components/Tests/Questions.vue";
 export default {
   name: "Test",
   components: {
-    MonacoEditor
-  },
-  data() {
-    return {
-      question:
-        "Find the maximum number in an array of integers. For example, in the array [1, -3, 5], the maximum number is 5.",
-      info: "Passed 2 out of 2",
-      options: {
-        //Monaco Editor Options
-      }
-    };
-  },
-  methods: {
-    onChange(value) {
-      console.log(value);
-    }
+    CodeQuestions,
+    Questions
   }
 };
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+}
+</style>
